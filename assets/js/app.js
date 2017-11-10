767e72b548ebc6d231943a521ba1f8b1da89fb54
Diana <dianachcastro@yahoo.es>
2017-11-10 10:42:30 -0300

    subiendo añadir lista

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+	function add(){
+//tomar el texto ingresado en el textarea
+	var tareas = document.getElementById('tarea').value;
+//limpiar el textarea
+	document.getElementById('tarea').value = '';
